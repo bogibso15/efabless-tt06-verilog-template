e66705cbae7c2964d0ee169e4c28609c0881fadf
LanceLander <161381350+LanceLander@users.noreply.github.com>
2024-04-09 11:33:10 -0400

    Update config.tcl
    variable name change

--- src/config.tcl
+++ src/config.tcl
@@ -74,5 +74,5 @@ set ::env(RUN_CTS) 1
 set ::env(CLOCK_PORT) {clk}
 
 # Don't use power rings or met5 layer
-set ::env(DESIGN_IS_CORE) 0
+set ::env(FP_PDN_MULTILAYER) 0
 set ::env(RT_MAX_LAYER) {met4}
